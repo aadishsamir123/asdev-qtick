90d56cb99b77f6b0fc4bdd2df2681ba3f16c67bb
aadishsamir123 <71179256+aadishsamir123@users.noreply.github.com>
2025-09-22 13:30:41 +0800

    removed unused images and also added favicon

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,7 +12,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 const config = {
   title: "QTick Documentation",
   tagline: "Smart QR Attendance Tracking - Simple, Fast, Reliable",
-  favicon: "img/favicon.ico",
+  favicon: "img/qtick-logo.png",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
